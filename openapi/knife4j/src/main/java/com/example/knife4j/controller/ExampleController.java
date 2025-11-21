22b6914578ab942bbd7f6a2549a5863f2faeaa6c
九思Jusi <jusi.bunny@gmail.com>
2025-11-21 17:56:44 +0800

    2025-11-21

--- openapi/knife4j/src/main/java/com/example/knife4j/controller/ExampleController.java
+++ openapi/knife4j/src/main/java/com/example/knife4j/controller/ExampleController.java
@@ -1,12 +1,16 @@
 package com.example.knife4j.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// @SecurityRequirement(name = HttpHeaders.AUTHORIZATION)
 @Slf4j
 @Tag(name = "示例接口")
 @RestController
@@ -15,8 +19,16 @@ public class ExampleController {
 
     @Operation(summary = "Hello World")
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(HttpServletRequest request) {
         log.info("Hello, World!");
+        log.info("请求头: {}", request.getHeader("Authorization"));
         return "Hello, World!";
+    }
+
+    @Operation(summary = "Hello Java")
+    @GetMapping("/helloJava")
+    public String helloJava() {
+        log.info("Hello, Java!");
+        return "Hello, Java!";
     }
 }
