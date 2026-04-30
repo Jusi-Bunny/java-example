6e675c231bd3a60377a9f682d61d84b278402668
九思Jusi <jusi.bunny@gmail.com>
2026-04-30 17:57:10 +0800

    2026-04-30

--- mybatis-plus/src/test/java/com/example/mybatis/MybatisPlusApplicationTests.java
+++ mybatis-plus/src/test/java/com/example/mybatis/MybatisPlusApplicationTests.java
@@ -1,65 +1,12 @@
 package com.example.mybatis;
 
-import com.example.mybatis.entity.User;
-import com.example.mybatis.mapper.UserMapper;
-import com.example.mybatis.service.UserService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
 
 @SpringBootTest
 class MybatisPlusApplicationTests {
 
-    @Autowired
-    private UserMapper userMapper;
-
     @Test
-    public void testSelectList() {
-        System.out.println(("----- selectAll method test ------"));
-        List<User> userList = userMapper.selectList(null);
-        userList.forEach(System.out::println);
-    }
-
-    @Test
-    public void testSelectById() {
-        System.out.println(("----- selectById method test ------"));
-        User user = userMapper.selectById(1L);
-        System.out.println(user);
-    }
-
-    @Test
-    public void testInsert() {
-        System.out.println(("----- insert method test ------"));
-        User user = new User();
-        user.setName("Joi");
-        user.setAge(18);
-        user.setEmail("joi@example.com");
-        int result = userMapper.insert(user);
-        System.out.println("result = " + result);
-    }
-
-    @Test
-    public void testUpdateById() {
-        System.out.println(("----- updateById method test ------"));
-        User user = userMapper.selectById(1L);
-        user.setName("XiaoMing");
-        // 修改
-        userMapper.updateById(user);
-    }
-
-    @Test
-    public void testDeleteById() {
-        System.out.println(("----- deleteById method test ------"));
-        int result = userMapper.deleteById(1L);
-        System.out.println("result = " + result);
-    }
-
-    @Test
-    public void testSelectUserById() {
-        System.out.println(("----- selectUserById method test ------"));
-        User user = userMapper.selectUserById(1L);
-        System.out.println(user);
+    void contextLoads() {
     }
 }
---
+++ mybatis-plus/src/test/java/com/example/mybatis/WrapperTest.java
@@ -0,0 +1,42 @@
+package com.example.mybatis;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.example.mybatis.entity.User;
+import com.example.mybatis.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class WrapperTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void testQueryWrapper() {
+        // 查询年龄大于20的
+        List<User> userList = userService.list(new QueryWrapper<User>().gt("age", 20));
+        userList.forEach(System.out::println);
+    }
+
+    @Test
+    public void testLambdaQueryWrapper() {
+        // 获取年龄大于20的
+        List<User> userList = userService.list(new QueryWrapper<User>().lambda().gt(User::getAge, 20));
+        userList.forEach(System.out::println);
+
+        LambdaQueryWrapper<User> gt = new LambdaQueryWrapper<>(User.class).gt(User::getAge, 20);
+
+    }
+
+    @Test
+    public void testWrappers() {
+        List<User> userList = userService.list(Wrappers.lambdaQuery(User.class).gt(User::getAge, 20));
+        userList.forEach(System.out::println);
+    }
+}
