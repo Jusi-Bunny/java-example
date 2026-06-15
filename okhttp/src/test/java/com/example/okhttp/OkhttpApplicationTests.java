074b1f8c7fff21f005a0f42ea1ff67f3f199221d
九思Jusi <jusi.bunny@gmail.com>
2026-06-15 22:42:44 +0800

    2026-06-15

--- okhttp/src/test/java/com/example/okhttp/OkhttpApplicationTests.java
+++ okhttp/src/test/java/com/example/okhttp/OkhttpApplicationTests.java
@@ -1,13 +1,78 @@
 package com.example.okhttp;
 
+import okhttp3.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class OkhttpApplicationTests {
 
+    private static final OkHttpClient CLIENT = new OkHttpClient();
+
+    private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+
+    /**
+     * GET 请求
+     */
     @Test
-    void contextLoads() {
+    public void okHttpGetDemo() throws IOException {
+
+        Request request = new Request.Builder()
+                .url("http://httpbin.org/get")
+                .get()
+                .build();
+
+        try (Response response = CLIENT.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new RuntimeException("请求失败，状态码：" + response.code());
+            }
+
+            String result = response.body().string();
+            System.out.println(result);
+        }
     }
 
+    /**
+     * 带 Header 的请求
+     */
+    @Test
+    public void okHttpHeaderDemo() throws IOException {
+        Request request = new Request.Builder()
+                .url("http://httpbin.org/get")
+                .addHeader("Authorization", "Bearer xxx")
+                .addHeader("Content-Type", "application/json")
+                .get()
+                .build();
+    }
+
+    /**
+     * POST 请求
+     */
+    @Test
+    public void okHttpPostDemo() throws IOException {
+        String json = """
+                {
+                  "username": "tom",
+                  "age": 18
+                }
+                """;
+
+        RequestBody requestBody = RequestBody.create(json, JSON);
+
+        Request request = new Request.Builder()
+                .url("http://httpbin.org/post")
+                .post(requestBody)
+                .build();
+
+        try (Response response = CLIENT.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new RuntimeException("请求失败，状态码：" + response.code());
+            }
+
+            String result = response.body().string();
+            System.out.println(result);
+        }
+    }
 }
